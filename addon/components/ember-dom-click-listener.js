09d1c16b699c45ebff4f55379c4c80d112880c55
Jeffrey Luong <jjck1988@gmail.com>
2018-06-18 12:54:54 -0400

    [MAINT] Adds integration tests

--- addon/components/ember-dom-click-listener.js
+++ addon/components/ember-dom-click-listener.js
@@ -15,44 +15,44 @@ const noParentErrorMssage = 'A `parentSelector` is required to check if a click 
 const allowedSelectorsErrorMessage = '`allowedSelectors` must be an array of selectors.';
 
 /**
-  A simple component that detects and handles clicks outside of target element(s).
-
-  @class EmberDomClickListener
-  @public
-*/
+ * A simple component that detects and handles clicks outside of target element(s).
+ *
+ * @class EmberDomClickListener
+ * @public
+ */
 
 export default Component.extend({
 
   /**
-    Holds the parent element selector name.
-
-    @property parentSelector
-    @type {String}
-    @default ''
-    @public
+  * Holds the parent element selector name.
+  *
+  * @property parentSelector
+  * @type {String}
+  * @default ''
+  * @public
   */
 
   parentSelector: '',
 
   /**
-    Holds a list of allowed selector names to check against.
-    Useful if there are elements that you do not wish to fire the action.
-
-    @property allowedSelectors
-    @type {Array}
-    @default []
-    @public
-  */
+   * Holds a list of allowed selector names to check against.
+   * Useful if there are elements that you do not wish to fire the action.
+   *
+   * @property allowedSelectors
+   * @type {Array}
+   * @default []
+   * @public
+   */
 
   allowedSelectors: computed(() => []),
 
   /**
-    The parent element that provides the context in which is acceptable to click inside.
-
-    @property parentElement
-    @type {String}
-    @public
-  */
+   * The parent element that provides the context in which is acceptable to click inside.
+   *
+   * @property parentElement
+   * @type {String}
+   * @public
+   */
 
   parentElement: computed('parentSelector', function() {
     let parentSelector = get(this, 'parentSelector');
@@ -63,11 +63,11 @@ export default Component.extend({
   }),
 
   /**
-    Removes click event outside the context of the component/view on destroy.
-
-    @method willDestroyElement
-    @public
-  */
+   * Removes click event outside the context of the component/view on destroy.
+   *
+   * @method willDestroyElement
+   * @public
+   */
 
   willDestroyElement() {
     this._super(...arguments);
@@ -75,11 +75,11 @@ export default Component.extend({
   },
 
   /**
-    Adds click event outside the context of the component/view.
-
-    @method didReceiveAttrs
-    @public
-  */
+   * Adds click event outside the context of the component/view.
+   *
+   * @method didReceiveAttrs
+   * @public
+   */
 
   didInsertElement() {
     this._super(...arguments);
@@ -90,20 +90,20 @@ export default Component.extend({
   },
 
   /**
-    Holds the action to trigger when clicking outside the context.
-    @property fireAction
-    @public
-  */
+   * Holds the action to trigger when clicking outside the context.
+   * @property fireAction
+   * @public
+   */
 
   fireAction() {},
 
   /**
-    The click handle method to resets the toggled property and removes
-    click listener if clicked outside.
-    @method clickHandler
-    @param {Object} event
-    @public
-  */
+   * The click handle method to resets the toggled property and removes
+   * click listener if clicked outside.
+   * @method clickHandler
+   * @param {Object} event
+   * @public
+   */
 
   clickHandler(event) {
     let allowedSelectors = get(this, 'allowedSelectors');
@@ -131,10 +131,10 @@ export default Component.extend({
   },
 
   /**
-    Adds click event outside the context of the component/view.
-    @method setDomClickEvent
-    @public
-  */
+   * Adds click event outside the context of the component/view.
+   * @method setDomClickEvent
+   * @public
+   */
 
   _setDomClickEvent() {
     return jquery(window).on(`click.${guidFor(this)}`, event => {
@@ -143,10 +143,10 @@ export default Component.extend({
   },
 
   /**
-    Removes click event outside the context of the component/view.
-    @method _removeDomClickEvent
-    @public
-  */
+   * Removes click event outside the context of the component/view.
+   * @method _removeDomClickEvent
+   * @public
+   */
 
   _removeDomClickEvent() {
     jquery(window).off(`click.${guidFor(this)}`);
